7cc07108176876ee4a5e097df834932d6de1f2f3
c0dingaddict <codingisabella@gmail.com>
2023-02-06 20:51:10 -0700

    Did extra credit

--- sorting-part.sh
+++ sorting-part.sh
@@ -5,6 +5,10 @@
 
 
 #### DECLARING VARIABLES
+
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
 end_file=".txt"
 file=$1
 last_four=${file: -4}
@@ -16,18 +20,13 @@ if [ "$1" != "" ]; then
         touch sorted.txt
         sort -o sorted.txt $@
     else
-        echo "File extension not allowed" 
+        echo -e "${RED}File extension not allowed" 
     fi
 
 else
 
     echo "Positional parameter 1 is empty"
 fi
-
-
-
-
-
 
 
 
